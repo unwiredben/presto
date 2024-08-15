80c03d979bd316452339dc21e901e3f69247dc71
Mike Bell <mdb036@gmail.com>
2024-08-15 11:50:06 +0100

    Fix colours

--- modules/c/presto/presto.cpp
+++ modules/c/presto/presto.cpp
@@ -153,7 +153,6 @@ static inline int32_t reverse(uint32_t x) {
     x = ((x >>  1) & 0x55555555u) | ((x & 0x55555555u) <<  1);
     x = ((x >>  2) & 0x33333333u) | ((x & 0x33333333u) <<  2);
     x = ((x >>  4) & 0x0f0f0f0fu) | ((x & 0x0f0f0f0fu) <<  4);
-    x = ((x >>  8) & 0x00ff00ffu) | ((x & 0x00ff00ffu) <<  8);
     x = ((x << 1) & 0xffc0ffc0) | (x & 0x003f003f); // drop the high B bit and merge
     return x;
 }
