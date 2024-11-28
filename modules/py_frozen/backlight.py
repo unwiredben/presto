07c109a25aa96d5676bf6d37e116a0b4e01896aa
Phil Howard <github@gadgetoid.com>
2024-11-28 16:01:17 +0000

    backlight: more optimisation.
    Collapse the reactive backlight update into a single loop.
    
    Remove the Zone class and use a locally scoped zone function to calculate
    the list of samples for each zone.
    
    Allow sample_size and sample_every to be tuned to balance speed and
    colour smoothness.

--- modules/py_frozen/backlight.py
+++ modules/py_frozen/backlight.py
@@ -2,65 +2,39 @@ import plasma
 import micropython
 
 
-class Zone(object):
-    SIZE = 80
-
-    # A section of the screen we'll use for sampling.
-    # We're using 80 x 80 sections and sampling 49 points within that space
-    def __init__(self, x, y, width=240):
-        self.colours = []
-
-        # Pre calculate the ranges to use later
-        # Calculates the coordinates in terms of RGB56 / two bytes per pixel
-        self.x_range = range(x * 2, (x + self.SIZE) * 2, 10 * 2)
-        self.y_range = range(y * 2, (y + self.SIZE) * 2, 10 * 2)
-
-        # Calculate the offsets for every pixel we need to sample
-        # A tuple is slightly less expensive than a list to iterate
-        self.pixels = tuple([y * width + x for y in self.y_range for x in self.x_range])
-        self.samples = len(self.pixels)
-
-    @micropython.native
-    def return_avg(self, mv):
-        # Running total of the RGB values for our average calculation later
-        r_value, g_value, b_value = 0, 0, 0
-
-        for offset in self.pixels:
-            # Grab the pixel colour value back from the framebuffer
-            # This will be in RGB565 - RRRRRGGGGGGBBBBB
-            rgb = (mv[offset] << 8) | mv[offset + 1]
-
-            # Now we need to sort and arrange those bits into their R G and B values
-            # We can't get back the exact value due to the destructive nature of converting to RGB565
-            # But it's close enough for displaying a colour on the backlight :)
-            # We're shifting the 5, 6 and 5 bits into the most significant places
-            r_value += (rgb >> 8) & 0b11111000
-            g_value += (rgb >> 3) & 0b11111100
-            b_value += (rgb << 3) & 0b11111000
-
-        # Now we finish the average calculation and return the value as an integer
-        return (
-            int(r_value / self.samples),
-            int(g_value / self.samples),
-            int(b_value / self.samples)
-        )
-
-
 class Reactive(object):
-    def __init__(self, surface, width=240, height=240):
+    # Map the seven backlight LEDs to zones in the display buffer.
+    # By default we're using 80 x 80 sections and sampling 64 points within that space.
+    # The sampled values are averaged to produce a final output colour.
+    def __init__(self, surface, width=240, height=240, sample_size=80, sample_every=10):
         self.mv = surface if isinstance(surface, memoryview) else memoryview(surface)
 
-        MAX = width - Zone.SIZE
-        MID = width // 2 - Zone.SIZE // 2
+        MAX = width - sample_size
+        MID = width // 2 - sample_size // 2
         MIN = 0
-        W = width
 
-        # The index for each section matches the index for the Backlight LED
-        # Zone 0 is in the bottom right corner LED as you're looking at the screen
-        # And the LEDs proceed around counter-clockwise
-        # A tuple is slightly less expensive than a list to iterate
-        self.zones = (Zone(MAX, MAX, W), Zone(MAX, MID, W), Zone(MAX, MIN, W), Zone(MID, MIN, W),
-                      Zone(MIN, MIN, W), Zone(MIN, MID, W), Zone(MIN, MAX, W))
+        # A section of the screen we'll use for sampling.
+        # Define this function locally so we don't need to pass width, sample_size and sample_every.
+        def zone(index, x, y):
+            # Calculate the X an dY coordinates in terms of RGB56 / two bytes per pixel
+            x_range = tuple(range(x * 2, (x + sample_size) * 2, sample_every * 2))
+            y_range = tuple(range(y * 2, (y + sample_size) * 2, sample_every * 2))
+
+            # Calculate the offsets for every pixel we need to sample.
+            # Coerce to a tuple, it's faster to iterate.
+            pixels = tuple([y * width + x for y in y_range for x in x_range])
+
+            return index, pixels
+
+        # The index for each section matches the index for the Backlight LED.
+        # Zone 0 is in the bottom right corner LED as you're looking at the screen.
+        # The LEDs proceed around the screen counter-clockwise.
+        # Use a tuple, it's faster to iterate.
+        self.zones = (zone(0, MAX, MAX), zone(1, MAX, MID), zone(2, MAX, MIN),
+                      zone(3, MID, MIN), zone(4, MIN, MIN), zone(5, MIN, MID),
+                      zone(6, MIN, MAX))
+
+        self.samples = len(self.zones[0][1])
 
         self.bl = plasma.WS2812(7, 0, 0, 33)
         self.bl.start()
@@ -68,8 +42,32 @@ class Reactive(object):
     @micropython.native
     def update(self):
         mv = self.mv
-        i = 0
-        # For each section we get the average colour in that area and set the backlight LED to that colour
-        for zone in self.zones:
-            self.bl.set_rgb(i, *zone.return_avg(mv))
-            i += 1
+        _ = self.samples  # It's repeatably faster with this line
+        # For each zone, get the average colour in that area and set the corresponding LED to that colour
+        for i, pixels in self.zones:
+            # Running total of the RGB values
+            r_value, g_value, b_value = 0, 0, 0
+
+            for offset in pixels:
+                # Grab the pixel colour value back from the framebuffer
+                # This will be in RGB565 - RRRRRGGGGGGBBBBB
+                # This is faster as 3 separate operations than a single line
+                rgb = mv[offset]
+                rgb <<= 8
+                rgb |= mv[offset + 1]
+
+                # Sort and arrange those bits into their approximate 8bit R G and B values.
+                # We can't get back the exact value due to the destructive nature of converting RGB888 to RGB565,
+                # but it's close enough for displaying a colour on the backlight :)
+                r_value += (rgb >> 8) & 0b11111000
+                g_value += (rgb >> 3) & 0b11111100
+                b_value += (rgb << 3) & 0b11111000
+
+            # Take the average of our values and set the backlight LED
+            # total // samples is faster than int(total / samples)
+            self.bl.set_rgb(
+                i,
+                r_value // self.samples,
+                g_value // self.samples,
+                b_value // self.samples
+            )
