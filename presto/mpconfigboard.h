f3764b636ef13b90b3678dbea1e2301538305648
Phil Howard <github@gadgetoid.com>
2024-11-28 14:41:11 +0000

    MicroPython: Remove core1_entry hack.

--- presto/mpconfigboard.h
+++ presto/mpconfigboard.h
@@ -33,6 +33,3 @@ int mp_hal_is_pin_reserved(int n);
 
 #define MICROPY_PY_THREAD                       (0)
 #define MICROPY_GC_SPLIT_HEAP                   (0)
-
-// TODO: Remove when https://github.com/micropython/micropython/pull/15655 is merged
-#define core1_entry                             (NULL)
